a3167fbce81df01951c3559b2acf72d6f7b61399
mutovkin_av <jw.buzd@gmail.com>
2022-03-31 17:54:04 +0300

    Дополнил 7 задание

--- ex_7.js
+++ ex_7.js
@@ -7,7 +7,10 @@ console.log(null == 0)
 console.log(null != 0)
 console.log(null > 0)
 console.log(null < 0)
-console.log(null + 0)
+console.log(null + 0) // при сложении, наверно, null был приведен к числовому типу
+console.log(null / 0)
+console.log(null * 0)
+console.log(0 / null)
 
 // null указывает на несуществующий объект — это просто специальное значение для понятия «ничто».
 // 0 - это числовой тип
