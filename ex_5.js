30fde1caeff594b45ab0deda98ebe1585e7296f3
mutovkin_av <jw.buzd@gmail.com>
2022-04-04 17:21:10 +0300

    Задание 5 решено

--- ex_5.js
+++ ex_5.js
@@ -16,3 +16,13 @@ for (let i = 0; i < 20; i++){
     }
     console.log(str);
 }
+
+// Второй вариант, вроде проще
+
+var x = ''
+for (var j = 0; j < 20; j++){
+    x += 'a'
+    console.log(x)
+}
+
+
